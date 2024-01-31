69fc7f3e1d65b6d8f1cc40b0d830459b5f2e8314
Leonard Weaver <leonard.weaver45@gmail.com>
2024-01-31 01:41:58 -0600

    Traffic generator update: Need to connect backend to traffic generator before this can move foreward

--- express-server/src/datasources/traffic.ts
+++ express-server/src/datasources/traffic.ts
@@ -1,92 +1,5 @@
 
-
-// async function RandomNumberGenerator() {
-//   const tick = 1000; // change tick speed
-//   //--------------------------------------------------------------------------------
-//   // Generate change in water level
-//   //--------------------------------------------------------------------------------
-//   // const [randomAmount, setRandomNumber1] = useState<number | null>(null);
-//   // const [directionNumber, setRandomNumber2] = useState<number | null>(null);
-//   // const finalChange = (randomAmount !== null && directionNumber !== null) ? (randomAmount * directionNumber).toFixed(1) : 'Calculating...';
-  
-//   // Function to generate a random number between 1 and 10
-//   // const generateRandomNumber = () => {
-//   //   const newRandomAmount = (Math.floor(Math.random() * 10) + 1) * 0.1;
-//   //   const newDirectionNumber = Math.floor(Math.random() * 3) - 1; // water level: increase +1, decrease -1, or stay the same 0
-//   //   setRandomNumber1(newRandomAmount);
-//   //   setRandomNumber2(newDirectionNumber);
-//   // };
-
-
-
-  
-
-
-
-//   //--------------------------------------------------------------------------------
-//   // Generate status changes on gates
-//   //--------------------------------------------------------------------------------
-
-//   // NOTE: Some of this will need to be moved to another file seeing as the traffic generator file will not exist in a real life deployment
-//   // NOTE: This is just a simulation for 1 gate at the moment, will need to be scaled for all gates
-//   const status = ['Green', 'Yellow', 'Red'];
-//   let randomStatus: number;
-//   let connectionError: boolean; // threat level red
-//   let lowBattery: boolean; // threat level yellow
-//   const waterLevel = ['Normal', 'High', 'Low']; // threat level yellow
-
-//   const chance = 3; // Change this variable to however often we want something to go wrong
-
-  
-
-//   const generateRandomAlert = () => {
-//     // Chance for something to go wrong
-//     const randomAlertChance = Math.floor(Math.random() * chance);
-
-//     randomStatus = randomAlertChance === 0 ? Math.floor(Math.random() * 2) + 1 : 0; // Choose an error to throw, 0 = no error, 1 = connection error, 2 = low battery
-
-//     // Set of errors
-//     connectionError = true ? randomStatus == 1 : false;
-//     lowBattery = true ? randomStatus == 2 : false;
-//     // TODO: Add more errors here
-//   }
-
-
-
-//   const callAllFunctions = () => {
-//     // generateRandomNumber();
-//     generateRandomAlert();
-//   }
-
-
-  // front end
-  // Use useEffect to automatically generate a new number every tick
-  // useEffect(() => {
-  //   const interval = setInterval(callAllFunctions, tick);
-  //   // 1000 milliseconds = 1 second
-  //   // Clear the interval when the component unmounts
-  //   return () => clearInterval(interval);
-  // }, []);
-
-
-
-
-
-
-  // return (
-  //   <div className="p-4">
-  //     <h1 className="text-2xl font-bold mb-4">Traffic Generator</h1>
-  //     <p className="text-lg mb-2">Change in water level: {finalChange}</p>
-  //   </div>
-  // );
-// }
-
-
-
-
-
-
-
+// Zack's notes: -----------------------------------------------------
 // Return array of objects 
 // ex 
 //  [
@@ -98,10 +11,72 @@
 // Run server 
 // Navigate to --> 
 // localhost:4001/api/v1/traffic/gen1
+// -------------------------------------------------------------------
+
 
 async function generateTraffic() {
   // Logic goes here : )
-  return "Hello World"
+
+  // TODO: Ultimately some of these values will need to be passed in from the front end, things like the fieldID, gateID, and optionally waterLevel can be updated within the generator, or the generator can provide a value to update the waterLevel with
+  // TODO the status(green, yellow, red) will likely be handled by something else that is not the traffic generator but for now it is included here
+  let fieldID = 1;
+  let gateID = 69;
+  let idealWaterLevel = 5; // this variable would be water the farmer sets it to be and will be passed in
+  let threshold = 1; // this variable would be the maximum allowed change in water level and will be passed in
+  let actualWaterLevel = 5; // this variable would be the actual water level and will be passed in
+  let connectionError = false;
+  let lowBattery = false;
+  let status = "Green";
+
+  const numOfErrors = 2; // Change this variable to however many errors we want to generate
+  const chance = 3; // Change this variable to however often we want something to go wrong
+  let randomStatus = 0;
+
+  //--------------------------------------------------------------------------------
+  //Generate change in water level
+  //--------------------------------------------------------------------------------
+  const changeWaterLevel = () => {
+    const randomAmount = (Math.floor(Math.random() * 10) + 1) * 0.1;
+    const directionNumber = Math.floor(Math.random() * 3) - 1; // water level: increase +1, decrease -1, or stay the same 0
+    const finalChange = (randomAmount * directionNumber).toFixed(1);
+    return finalChange
+  }
+  actualWaterLevel += parseFloat(changeWaterLevel());
+
+
+
+  //--------------------------------------------------------------------------------
+  // Generate status changes on gates
+  //--------------------------------------------------------------------------------
+  const randomAlertChance = Math.floor(Math.random() * chance);
+  // Choose an error to throw, 0 = no error, 1 = connection error, 2 = low battery
+  if (randomAlertChance === 0) {
+    randomStatus = Math.floor(Math.random() * numOfErrors) + 1
+  }
+  else {
+    randomStatus = 0
+  }
+
+  connectionError = true ? randomStatus == 1 : false;
+  lowBattery = true ? randomStatus == 2 : false;
+  // TODO: Add more errors here
+
+  // Determine status of gate
+  if (connectionError) {
+    status = "Red"
+  }
+  else if (lowBattery) {
+    status = "Yellow"
+  }
+  else if (actualWaterLevel > idealWaterLevel + threshold || actualWaterLevel < idealWaterLevel - threshold) {
+    status = "Yellow"
+  }
+  else {
+    status = "Green"
+  }
+
+  // gate = [fieldID, gateID, status, waterLevel, threshold, connectionError, lowBattery]
+  return status;
 }
 
 
