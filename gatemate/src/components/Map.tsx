d6c3b1e828b3ff3696b52a8b76cc469ed814dbca
Zeanderson <121685062+Zeanderson@users.noreply.github.com>
2024-01-31 20:19:00 -0600

    Updated Map :) - Deleted traffic.py not needed

--- gatemate/src/components/Map.tsx
+++ gatemate/src/components/Map.tsx
@@ -1,5 +1,7 @@
-import Map from 'react-map-gl';
-
+import {Map, Marker} from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWheatAwn } from '@fortawesome/free-solid-svg-icons';
 
 function GLMap() {
     return (
@@ -9,14 +11,55 @@ function GLMap() {
                 mapLib={import('mapbox-gl')}
                 initialViewState={{
                     longitude: -94.160583,
-                    latitude: 36.061932,
-                    zoom: 10,
+                    latitude: 32.061932,
+                    zoom: 14,
                 }}
-                style={{ width: 1300, height: 650 }}
-                mapStyle="mapbox://styles/mapbox/streets-v9"
-            />
+                style={{ width: 1300, height: 650, borderRadius: '15px', overflow: 'hidden'}}
+                mapStyle="mapbox://styles/mapbox/outdoors-v12"
+            >
+                <Marker
+                    style={{ position:'absolute'}}
+                     longitude={-94.160583}
+                     latitude={32.061932}
+                >
+                    <div className='flex flex-col gap-1'>
+                        <h1 className='text-xl text-black font-Arvo'>Field 1</h1>
+                        <button className='text-green-500' onClick={() => window.location.href = "/field"}>
+                            <FontAwesomeIcon icon={faWheatAwn} size="4x"/>
+                        </button>
+                    </div>
+                </Marker>
+
+                <Marker
+                    style={{ position:'absolute'}}
+                     longitude={-94.170583}
+                     latitude={32.063932}
+                >   
+                    <div className='flex flex-col gap-1'>
+                        <h1 className='text-xl text-black font-Arvo'>Field 2</h1>
+                        <button className='text-yellow-500' onClick={() => window.location.href = "/field"}>
+                            <FontAwesomeIcon icon={faWheatAwn} size="4x"/>
+                        </button>
+                    </div>
+                </Marker>
+
+                <Marker
+                    style={{ position:'absolute'}}
+                    longitude={-94.150583}
+                    latitude={32.062932}
+                >
+                    <div className='flex flex-col gap-1'>
+                        <h1 className='text-xl text-black font-Arvo'>Field 3</h1>
+                        <button className='text-red-500' onClick={() => window.location.href = "/field"}>
+                            <FontAwesomeIcon icon={faWheatAwn} size="4x"/>
+                        </button>
+                    </div>
+                </Marker>
+
+            </Map>
         </div>
     )
 }
+
 
 export default GLMap
