0af346a29f0e264983a19c5ccc5489f945053a33
Zeanderson <121685062+Zeanderson@users.noreply.github.com>
2024-02-07 18:50:14 -0600

    GUI UI mess-up

--- gatemate/src/components/UserBanner.tsx
+++ gatemate/src/components/UserBanner.tsx
@@ -1,24 +1,28 @@
-import { faBars, faGear, faDoorOpen } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faGear, faDoorOpen } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { Modal } from 'react-responsive-modal'
-import UserModal from './Modals';
-import 'react-responsive-modal/styles.css';
+import UserModal from "./Modals";
 
 type BannerProps = {
-    userName: string
-    settingsLink: string
-    signOutLink: string
-    className?: string
-    children?: React.ReactNode
-}
+  userName: string;
+  settingsLink: string;
+  signOutLink: string;
+  className?: string;
+  children?: React.ReactNode;
+};
 
 export function UserBanner(props: BannerProps) {
-    return (
-        <div className={"flex flex-row rounded-xl bg-indigo-950 gap-4 py-2 pl-5 pr-5 items-center " + props.className}>
-            <h1>{props.userName}</h1>
-            {/* <button onClick={() => { setOpen(!open) }}><FontAwesomeIcon icon={faBars} /></button> */}
-            <UserModal bgColor={'bg-gray-500'} settingsLink='/' signoutLink='/' />
-            {/* <div className={'flex flex-col'}>
+  return (
+    <div
+      className={
+        "flex flex-row rounded-xl bg-indigo-950 gap-4 py-2 pl-5 pr-5 items-center " +
+        props.className
+      }
+    >
+      <h1>{props.userName}</h1>
+      {/* <button onClick={() => { setOpen(!open) }}><FontAwesomeIcon icon={faBars} /></button> */}
+      <UserModal bgColor={"bg-gray-500"} settingsLink="/" signoutLink="/" />
+      {/* <div className={'flex flex-col'}>
                     <div className='flex gap-2 items-center'>
                         <Link to={props.settingsLink}>{"Settings"}</Link>
                         <FontAwesomeIcon icon={faGear} />
@@ -27,10 +31,9 @@ export function UserBanner(props: BannerProps) {
                         <Link to={props.signOutLink}>{"Sign Out"}</Link>
                         <FontAwesomeIcon icon={faDoorOpen} />
                     </div> */}
-            {/* </div> */}
-
-        </div>
-    )
+      {/* </div> */}
+    </div>
+  );
 }
 
-export default UserBanner; 
+export default UserBanner;
