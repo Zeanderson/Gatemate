6fa936f3c4a8475fde3d5fe625fa11910aa99ec1
Leonard Weaver <leonard.weaver45@gmail.com>
2024-02-19 18:16:19 -0600

    Traffic Generator should now return mock data for 3 gates

--- express-server/src/datasources/traffic.ts
+++ express-server/src/datasources/traffic.ts
@@ -10,7 +10,7 @@
 // How to test : 
 // Run server 
 // Navigate to --> 
-// localhost:4001/api/v1/traffic/gen1
+// http://localhost:4001/api/v1/traffic/gen1?userId=1&fieldId=60
 // -------------------------------------------------------------------
 
 type Gate = {
@@ -32,90 +32,84 @@ type TrafficReturn = {
 async function generateTraffic(userId: string, fieldId: string) {
   // TODO: Ultimately some of these values will need to be passed in, things like waterLevel can be updated within the generator, or the generator can provide a value to update the waterLevel with
   // TODO the status(green, yellow, red) will likely be handled by something else that is not the traffic generator but for now it is included here
-  let gateId = 1; // TODO: Assume there are 3 gates, fix later
   let idealWaterLevel = 5; // this variable would be water the farmer sets it to be and will be passed in
   let threshold = 2; // this variable would be the maximum allowed change in water level and is set by the farmer and will be passed in
-  // TODO: Call up to the database to get the users preferred water level and threshold from the usersId from mongoDB
-  let actualWaterLevel = idealWaterLevel; // this variable will start out as the ideal water level and will be randomly updated by the generator
-  let connectionError = false;
-  let lowBattery = false;
-  let status = "Green";
 
-  const numOfErrors = 2; // Change this variable to however many errors we want to generate e.g right now it is 2 because we have connection error and low battery, if we added another error possibility we would change this to 3
-  const chance = 3; // Change this variable to however often we want something to go wrong
-  let randomStatus = 0;
+  const gates = [];
 
-  //--------------------------------------------------------------------------------
-  //Generate change in water level
-  //--------------------------------------------------------------------------------
-  const changeWaterLevel = () => {
-    const randomAmount = (Math.floor(Math.random() * 15) + 1) * 0.1 * threshold;
-    const directionNumber = Math.floor(Math.random() * 3) - 1; // water level: increase +1, decrease -1, or stay the same 0
-    const finalChange = (randomAmount * directionNumber).toFixed(1);
-    return finalChange
-  }
-  actualWaterLevel += parseFloat(changeWaterLevel());
+  for (let gateId = 0; gateId < 3; gateId++) {
+    
+    // TODO: Call up to the database to get the users preferred water level and threshold from the usersId from mongoDB
+    let actualWaterLevel = idealWaterLevel; // this variable will start out as the ideal water level and will be randomly updated by the generator
+    let connectionError = false;
+    let lowBattery = false;
+    let status = "Green";
 
+    const numOfErrors = 2; // Change this variable to however many errors we want to generate e.g right now it is 2 because we have connection error and low battery, if we added another error possibility we would change this to 3
+    const chance = 3; // Change this variable to however often we want something to go wrong
+    let randomStatus = 0;
 
-
-  //--------------------------------------------------------------------------------
-  // Generate status changes on gates
-  //--------------------------------------------------------------------------------
-  const randomAlertChance = Math.floor(Math.random() * chance);
-  // Choose an error to throw, 0 = no error, 1 = connection error, 2 = low battery
-  if (randomAlertChance === 0) {
-    randomStatus = Math.floor(Math.random() * numOfErrors) + 1
-  }
-  else {
-    randomStatus = 0
-  }
-
-  connectionError = true ? randomStatus == 1 : false;
-  lowBattery = true ? randomStatus == 2 : false;
-  // TODO: Add more errors here
-
-  // Determine status of gate
-  if (connectionError) {
-    status = "Red"
-  }
-  else if (lowBattery) {
-    status = "Yellow"
-  }
-  else if (actualWaterLevel > idealWaterLevel + threshold || actualWaterLevel < idealWaterLevel - threshold) {
-    status = "Yellow"
-  }
-  else {
-    status = "Green"
-  }
+    //--------------------------------------------------------------------------------
+    //Generate change in water level
+    //--------------------------------------------------------------------------------
+    const changeWaterLevel = () => {
+      const randomAmount = (Math.floor(Math.random() * 15) + 1) * 0.1 * threshold;
+      const directionNumber = Math.floor(Math.random() * 3) - 1; // water level: increase +1, decrease -1, or stay the same 0
+      const finalChange = (randomAmount * directionNumber).toFixed(1);
+      return finalChange
+    };
+    actualWaterLevel += parseFloat(changeWaterLevel());
 
 
-  
-  //--------------------------------------------------------------------------------
-  // Zacks stuff - use this
-  //--------------------------------------------------------------------------------
+    //--------------------------------------------------------------------------------
+    // Generate status changes on gates
+    //--------------------------------------------------------------------------------
+    const randomAlertChance = Math.floor(Math.random() * chance);
+    // Choose an error to throw, 0 = no error, 1 = connection error, 2 = low battery
+    if (randomAlertChance === 0) {
+      randomStatus = Math.floor(Math.random() * numOfErrors) + 1
+    }
+    else {
+      randomStatus = 0
+    }
 
-  //2-13-24
-  //Assume all fields have 3 gates
-  //gateId should start at 0 and go to 2
+    connectionError = true ? randomStatus == 1 : false;
+    lowBattery = true ? randomStatus == 2 : false;
+    // TODO: Add more errors here
 
-  const mockData = {
-    gateId: gateId,
-    idealWaterLevel: idealWaterLevel,
-    threshold: threshold,
-    actualWaterLevel: actualWaterLevel,
-    connectionError: connectionError,
-    lowBattery: lowBattery,
-    status: status
+    // Determine status of gate
+    if (connectionError) {
+      status = "Red"
+    }
+    else if (lowBattery) {
+      status = "Yellow"
+    }
+    else if (actualWaterLevel > idealWaterLevel + threshold || actualWaterLevel < idealWaterLevel - threshold) {
+      status = "Yellow"
+    }
+    else {
+      status = "Green"
+    }
+
+    const gateData = {
+      gateId: gateId,
+      idealWaterLevel: idealWaterLevel,
+      threshold: threshold,
+      actualWaterLevel: actualWaterLevel,
+      connectionError: connectionError,
+      lowBattery: lowBattery,
+      status: status
+    };
+    gates.push(gateData);
   }
 
 
   const traffic = {
     userId: userId,
     fieldId: fieldId,
-    gates: [
-      mockData
-    ],
-  }
+    gates: gates,
+  };
+
   return traffic;
 }
 
