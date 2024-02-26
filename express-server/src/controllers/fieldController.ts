6509042f5f1be2b47cdf055a8bf4d38ced5f6ef0
Levi Crider <levicrider08@gmail.com>
2024-02-26 11:01:11 -0600

    Field controller is working

--- express-server/src/controllers/fieldController.ts
+++ express-server/src/controllers/fieldController.ts
@@ -13,10 +13,18 @@ fieldRouter.post("/create", async (req: Request, res: Response) => {
     const user = await User.findOne({ email: req.session.user.email });
     if (!user) return res.status(404).send("User not found");
 
-    const field = new Field(req.body);
-    user.fields.push(field);
+    const field = new Field({
+      fieldId: user.fields.length + 1, // Auto generate fieldId based on the number of existing fields
+      location: req.body.location.map((coord: [number, number]) => ({
+        lat: coord[0],
+        lon: coord[1],
+      })),
+      Gates: [],
+    });
 
+    user.fields.push(field);
     await user.save();
+
     res.status(201).json(field);
   } catch (err) {
     res.status(500).send(err);
@@ -26,6 +34,7 @@ fieldRouter.post("/create", async (req: Request, res: Response) => {
 // Get all fields for a specific user
 fieldRouter.get("/", async (req: Request, res: Response) => {
   try {
+    console.log(req.session);
     if (!req.session?.user) {
       return res.status(403).send("User not logged in");
     }
@@ -61,27 +70,27 @@ fieldRouter.get("/:fieldId", async (req: Request, res: Response) => {
 });
 
 // Update a field by id
-fieldRouter.put("/:fieldId", async (req: Request, res: Response) => {
-  try {
-    if (!req.session?.user) {
-      return res.status(403).send("User not logged in");
-    }
+// fieldRouter.put("/:fieldId", async (req: Request, res: Response) => {
+//   try {
+//     if (!req.session?.user) {
+//       return res.status(403).send("User not logged in");
+//     }
 
-    const user = await User.findOne({ email: req.session.user.email });
-    if (!user) return res.status(404).send("User not found");
+//     const user = await User.findOne({ email: req.session.user.email });
+//     if (!user) return res.status(404).send("User not found");
 
-    const fieldId = Number(req.params.fieldId);
-    const field = user.fields.find((field) => field.fieldId === fieldId);
-    if (!field) return res.status(404).send("Field not found");
+//     const fieldId = Number(req.params.fieldId);
+//     const field = user.fields.find((field) => field.fieldId === fieldId);
+//     if (!field) return res.status(404).send("Field not found");
 
-    Object.assign(field, req.body);
-    await user.save();
+//     Object.assign(field, req.body);
+//     await user.save();
 
-    res.json(field);
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
+//     res.json(field);
+//   } catch (err) {
+//     res.status(500).send(err);
+//   }
+// });
 
 // Delete a field by id
 fieldRouter.delete("/:fieldId", async (req: Request, res: Response) => {
